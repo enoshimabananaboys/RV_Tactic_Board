49fc5ffa7bcb7f33a26d6d11e6361b3ef9e1d4e6
banana <enoshimabananaboys@gmail.com>
2026-09-22 11:24:40 +0900

    update:コメント追加

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 公開ファイルを変更したらバージョンを更新する。scopeごとにキャッシュを分離する。
 const PREFIX = "rv-tactic-board-" + self.registration.scope + "-";
-const CACHE = PREFIX + "v24";
+const CACHE = PREFIX + "v25";
 const ASSETS = [
   "./",
   "./index.html",
